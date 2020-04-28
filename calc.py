2fd6428652bf4da38ad6bfe42e5f860a40ba192e
Warisul Imam <warisulimam@gmail.com>
2020-04-28 16:37:49 +0600

    Added flexibility to display and button structure. Added display and button frames for better management.

--- calc.py
+++ calc.py
@@ -4,6 +4,7 @@ from math import sqrt
 
 root = Tk()
 root.title('Calculator of Opovrongsho') #Don't mind the queer choice of name
+root.geometry('300x500')
 
 #Key Icons Below
 delete_icon = PhotoImage(file="C:/Users/Administrator/Desktop/Program Files/Calculator Project/TkinterCalc/deleteKeyIcon.gif")
@@ -12,23 +13,31 @@ delete_icon = PhotoImage(file="C:/Users/Administrator/Desktop/Program Files/Calc
 memory = [0]
 operationmem = ''
 
+#Defining and Placing Frames
+displayFrame = Frame(root)
+displayFrame.place(relwidth=1, relheight=.15, anchor='nw')
 
-display = Label(root, bg='#B7DCFB', text='', width=31, height=3, font=('', 13))
-display.grid(row=0, column=0, columnspan=3, sticky='n s')
+buttonFrame = Frame(root, bg='blue')
+buttonFrame.place(relx=0, rely=.15, relwidth=1, relheight=.85)
+
+
+#Defining and Placing the Display of the Calculator
+display = Label(displayFrame, bg='#B7DCFB', text='', font=('', 18))
+display.place(relwidth=1, relheight=1)
 
 
 #Numerical Buttons Defined
 button_width, button_height = 12, 3
-button1 = Button(root, text='1', width=button_width, height=button_height, pady=5, command=lambda:log(button1['text']))
-button2 = Button(root, text='2', width=button_width, height=button_height, pady=5, command=lambda:log(button2['text']))
-button3 = Button(root, text='3', width=button_width, height=button_height, pady=5, command=lambda:log(button3['text']))
-button4 = Button(root, text='4', width=button_width, height=button_height, pady=5, command=lambda:log(button4['text']))
-button5 = Button(root, text='5', width=button_width, height=button_height, pady=5, command=lambda:log(button5['text']))
-button6 = Button(root, text='6', width=button_width, height=button_height, pady=5, command=lambda:log(button6['text']))
-button7 = Button(root, text='7', width=button_width, height=button_height, pady=5, command=lambda:log(button7['text']))
-button8 = Button(root, text='8', width=button_width, height=button_height, pady=5, command=lambda:log(button8['text']))
-button9 = Button(root, text='9', width=button_width, height=button_height, pady=5, command=lambda:log(button9['text']))
-button0 = Button(root, text='0', width=button_width, height=button_height, pady=5, command=lambda:log(button0['text']))
+button1 = Button(buttonFrame, text='1', width=button_width, height=button_height, pady=5, command=lambda:log(button1['text']))
+button2 = Button(buttonFrame, text='2', width=button_width, height=button_height, pady=5, command=lambda:log(button2['text']))
+button3 = Button(buttonFrame, text='3', width=button_width, height=button_height, pady=5, command=lambda:log(button3['text']))
+button4 = Button(buttonFrame, text='4', width=button_width, height=button_height, pady=5, command=lambda:log(button4['text']))
+button5 = Button(buttonFrame, text='5', width=button_width, height=button_height, pady=5, command=lambda:log(button5['text']))
+button6 = Button(buttonFrame, text='6', width=button_width, height=button_height, pady=5, command=lambda:log(button6['text']))
+button7 = Button(buttonFrame, text='7', width=button_width, height=button_height, pady=5, command=lambda:log(button7['text']))
+button8 = Button(buttonFrame, text='8', width=button_width, height=button_height, pady=5, command=lambda:log(button8['text']))
+button9 = Button(buttonFrame, text='9', width=button_width, height=button_height, pady=5, command=lambda:log(button9['text']))
+button0 = Button(buttonFrame, text='0', width=button_width, height=button_height, pady=5, command=lambda:log(button0['text']))
 
 blist = [button1['text'], button2['text'], button3['text'], button4['text'], button5['text'], button6['text'], button7['text'], button8['text'], button9['text'], button0['text']]
 
@@ -119,41 +128,17 @@ def equals():
 			display['text'] = str(x)
 
 #Operational Buttons Defined
-buttton_add = Button(root, text='+', width=button_width, height=button_height, pady=5, command=add)
-button_substract = Button(root, text='_', width=button_width, height=button_height, pady=5, command=substract)
-button_multiply = Button(root, text='*', width=button_width, height=button_height, pady=5, command=multiply)
-button_divide = Button(root, text='/', width=button_width, height=button_height, pady=5, command=divide)
-button_sqrt = Button(root, text='√', width=button_width, height=button_height, pady=5, command=rooot)
-button_equal = Button(root, text='=', padx=133.5, height=button_height, pady=5, command=equals)
-clr = Button(root, text='CLR',height=button_height, width=button_width, pady=5, command=clear).grid(row=6, column=0, sticky='w')
-button_decimal = Button(root, text='.', width=button_width, height=button_height, pady=5, command=point)
-delete_button = Button(root, image=delete_icon, width=88, height=58, command=delete)
+buttton_add = Button(buttonFrame, text='+', width=button_width, height=button_height, pady=5, command=add)
+button_substract = Button(buttonFrame, text='_', width=button_width, height=button_height, pady=5, command=substract)
+button_multiply = Button(buttonFrame, text='*', width=button_width, height=button_height, pady=5, command=multiply)
+button_divide = Button(buttonFrame, text='/', width=button_width, height=button_height, pady=5, command=divide)
+button_sqrt = Button(buttonFrame, text='√', width=button_width, height=button_height, pady=5, command=rooot)
+button_equal = Button(buttonFrame, text='=', padx=133.5, height=button_height, pady=5, command=equals)
+clr = Button(buttonFrame, text='CLR',height=button_height, width=button_width, pady=5, command=clear)
+button_decimal = Button(buttonFrame, text='.', width=button_width, height=button_height, pady=5, command=point)
+delete_button = Button(buttonFrame, image=delete_icon, width=88, height=58, command=delete)
 
-
-#Packing the Buttons
-button7.grid(row=1, column=0, sticky='w')
-button8.grid(row=1, column=1, sticky='n s')
-button9.grid(row=1, column=2, sticky='e')
-
-button4.grid(row=2, column=0, sticky='w')
-button5.grid(row=2, column=1, sticky='n s')
-button6.grid(row=2, column=2, sticky='e')
-
-button1.grid(row=3, column=0, sticky='w')
-button2.grid(row=3, column=1, sticky='n s')
-button3.grid(row=3, column=2, sticky='e')
-
-button0.grid(row=4, column=1, sticky='n s')
-
-buttton_add.grid(row=4, column=2, sticky='e')
-button_substract.grid(row=5, column=2, sticky='e')
-button_multiply.grid(row=4, column=0, sticky='w')
-button_divide.grid(row=5, column=0, sticky='w')
-button_sqrt.grid(row=5, column=1, sticky='n s')
-button_equal.grid(row=7, column=0, columnspan=3, sticky='n s')
-button_decimal.grid(row=6, column=1, sticky='n s')
-delete_button.grid(row=6, column=2, sticky='e')
-
+#Placing the Buttons
 
 
 root.mainloop()
